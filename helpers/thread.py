cfaa1f6cc6b0a17952f683a0a5447144a4eb0f0b
Daan Van Robays <daan.vanrobays@mediahuis.be>
2024-08-02 17:00:24 +0200

    Increase cv2 timeout and implement retry

--- helpers/thread.py
+++ helpers/thread.py
@@ -1,10 +1,14 @@
 import cv2, threading, queue
+import time
 
 
 class ThreadingClass:
     # initiate threading class
     def __init__(self, name):
         self.cap = cv2.VideoCapture(name)
+        self.stream_url = name
+        self.cap.set(cv2.CAP_PROP_OPEN_TIMEOUT_MSEC, 60000)
+        self.cap.set(cv2.CAP_PROP_READ_TIMEOUT_MSEC, 60000)
         # define an empty queue and thread
         self.q = queue.Queue()
         t = threading.Thread(target=self._reader)
@@ -14,10 +18,25 @@ class ThreadingClass:
     # read the frames as soon as they are available
     # this approach removes OpenCV's internal buffer and reduces the frame lag
     def _reader(self):
+        retry_count = 0
+        retry_delay = 5
+        max_retries = 5
         while True:
             ret, frame = self.cap.read()  # read the frames and ---
             if not ret:
-                break
+                print("Error: Stream timeout or frame read error.")
+                retry_count += 1
+                if retry_count > max_retries:
+                    print("Error: Maximum retry limit reached. Exiting.")
+                    break
+                else:
+                    print(f"Retrying in {retry_delay} seconds... ({retry_count}/{max_retries})")
+                    time.sleep(retry_delay)
+                    self.cap.release()
+                    self.cap = cv2.VideoCapture(self.stream_url)
+                    continue
+            else:
+                retry_count = 0
             if not self.q.empty():
                 try:
                     self.q.get_nowait()
